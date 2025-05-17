e7ba5f007a167d8642174009e89e12b024f5afc6
erik <hgrootoonk@proton.me>
2025-05-17 15:51:53 +0200

    different nvim configs added

---
+++ minimalnvim/nvim/lua/vim-options.lua
@@ -0,0 +1,26 @@
+-- set nofsync
+--vim.g.nofsync = true
+
+-- set tab behaviour
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+-- vim.cmd("colorscheme unokai")
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>v', '<C-v>')
+vim.keymap.set('n', 'x', '"_x')
+vim.opt.swapfile = false
+vim.opt.clipboard = "unnamedplus"
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.opt.termguicolors = true
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>v', '<C-v>')
+vim.keymap.set('n', 'x', '"_x')
