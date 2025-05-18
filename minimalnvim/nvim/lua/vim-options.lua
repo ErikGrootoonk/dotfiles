dc097a142c0ec234730edf81e1d3e5e0ce404400
erik <hgrootoonk@proton.me>
2025-05-18 16:00:14 +0200

    several plugin updates

--- minimalnvim/nvim/lua/vim-options.lua
+++ minimalnvim/nvim/lua/vim-options.lua
@@ -2,18 +2,32 @@
 --vim.g.nofsync = true
 
 -- set tab behaviour
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
--- vim.cmd("colorscheme unokai")
-vim.g.mapleader = " "
+-- vim.cmd('set expandtab')
+-- vim.cmd('set tabstop=4')
+-- vim.cmd('set softtabstop=4')
+-- vim.cmd('set shiftwidth=4')
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.o.showmode = false
 vim.keymap.set('n', '<leader>v', '<C-v>')
-vim.keymap.set('n', 'x', '"_x')
 vim.opt.swapfile = false
-vim.opt.clipboard = "unnamedplus"
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.splitright = true
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.wo.number = true
-vim.wo.relativenumber = true
+vim.wo.relativenumber = false
 vim.opt.termguicolors = true
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
@@ -21,6 +35,36 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('n', '<leader>v', '<C-v>')
-vim.keymap.set('n', 'x', '"_x')
+
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
