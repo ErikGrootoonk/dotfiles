deb9b2467ea9837c3a23c163f20c2be50bcc5d87
Erik <hgrootoonk@proton.me>
2025-06-29 21:53:47 +0200

    neovim update script added

---
+++ nvim/updateNvim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# File path to check
+FILE="$HOME/downloads/nvim-linux-x86_64.tar.gz"
+
+# Check if file exists
+if [ -f "$FILE" ]; then
+    echo "Found $FILE"
+    echo "Deleting file..."
+    rm "$FILE"
+fi
+
+curl  -O --output-dir ~/downloads https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz ~/downloads/
+
+sudo rm -rf /opt/nvim-linux-x86_64
+
+sudo tar -C /opt -xzf $FILE
