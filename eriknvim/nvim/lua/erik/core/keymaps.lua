9a60ae22ae91b2db3f39605ef71f05c27756e847
Erik <erik.grootoonk@gmail.com>
2025-12-16 19:08:20 +0100

    v keymap eriknvim

--- eriknvim/nvim/lua/erik/core/keymaps.lua
+++ eriknvim/nvim/lua/erik/core/keymaps.lua
@@ -5,3 +5,6 @@ vim.keymap.set('n', '<c-k>', ':wincmd k<cr>', {desc = 'move to pane up'})
 vim.keymap.set('n', '<c-l>', ':wincmd l<cr>', {desc = 'move to pane right'})
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('n', '<leader>w', ':w<cr>', {desc = 'save file with leader w'}) -- Save file
+
+-- Block V select in windows
+vim.keymap.set('n', '<leader>v', '<C-v', {desc = 'enable block select in wsl'})
