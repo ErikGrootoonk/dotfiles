df6c65bb5ce99942f20537a96a7edf37c86ef1bb
ErikG <erik.grootoonk@pinkroccade.nl>
2026-01-22 07:51:54 +0100

    updates

--- eriknvim/nvim/lua/erik/core/options.lua
+++ eriknvim/nvim/lua/erik/core/options.lua
@@ -5,6 +5,18 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.background = "dark"
 vim.opt.swapfile = false
-vim.wo.number = true
-vim.opt.signcolumn = yes
-
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.updatetime = 250
+vim.opt.list = true
+vim.opt.listchars = { tab = ">> ", trail = "." } -- kan symbool voor nbsp niet vinden kan ook hierbij
+vim.opt.inccommand = "split"
+vim.opt.scrolloff = 10
