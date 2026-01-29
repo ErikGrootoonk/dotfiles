155a71509832cb13799967b2a1c43af76a66c2df
Erik Grootoonk <erik.grootoonk@pinkroccade.nl>
2026-01-29 16:21:23 +0100

    plugins installed

--- eriknvim/nvim/lua/config/lazy.lua
+++ eriknvim/nvim/lua/config/lazy.lua
@@ -21,6 +21,8 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+require("config.options")
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
@@ -34,4 +36,4 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
-require("config.options")
+require("config.keymaps")
