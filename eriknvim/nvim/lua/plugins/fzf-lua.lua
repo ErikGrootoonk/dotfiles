7b66b2eaf01a52415cf49563bbc3290828451ebb
ErikG <erik.grootoonk@gmail.com>
2026-01-30 22:20:34 +0100

    fzf verder geconfigureerd

--- eriknvim/nvim/lua/plugins/fzf-lua.lua
+++ eriknvim/nvim/lua/plugins/fzf-lua.lua
@@ -17,7 +17,32 @@ return {
     { "<leader>fg", 
     function() require('fzf-lua').live_grep()  end,
     desc="Find by grepping in project",
-    }
+    },
+    { "<leader>fc", 
+    function() require('fzf-lua').files({cwd=vim.fn.stdpath("config")})  end,
+    desc="[f]ind in neovim [c]onfiguration",
+    },
+    { "<leader>fk", 
+    function() require('fzf-lua').keymaps()  end,
+    desc="Find your keymaps",
+    },
+    { "<leader>fo", 
+    function() require('fzf-lua').oldfiles()  end,
+    desc="Find Old files",
+    },
+    { "<leader>fw", 
+    function() require('fzf-lua').grep_cword()  end,
+    desc="Find words",
+    },
+    { "<leader><leader>", 
+    function() require('fzf-lua').buffers()  end,
+    desc="show buffers",
+    },
+    { "<leader>/", 
+    function() require('fzf-lua').lgrep_curbuf()  end,
+    desc="[/] Live grep the current buffer",
+    },
+
   }
 
 }
