73a0e26fad370eac2205250599521290a7d6f9fb
Erik Grootoonk <erik.grootoonk@pinkroccade.nl>
2026-02-06 11:59:05 +0100

    small update

--- barenvim/nvim/init.lua
+++ barenvim/nvim/init.lua
@@ -78,10 +78,10 @@ vim.opt.listchars = {
 }
 
 -- netrw config
---g.netrw_banner = 0	    			-- gets rid of the annoying banner for netrw
+g.netrw_banner = 0	    			-- gets rid of the annoying banner for netrw
 --g.netrw_browse_split = 4
 --g.netrw_altv = 0					-- change from left splitting to right splitting
---g.netrw_liststyle = 3				-- tree style view in netrw
+g.netrw_liststyle = 3				-- tree style view in netrw
 ----vim.g.netrw_winsize = 30                -- takes up 30% of the screen
 --g.netrw_sizestyle = "H"
 --g.netrw_sort_sequence = [[[\/]$,*]] -- sort directories first
