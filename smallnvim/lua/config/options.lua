1ab644a13713ad54418302d778e0cc461ba9e823
ErikG <erik.grootoonk@gmail.com>
2026-02-17 19:37:07 +0100

    small update

--- smallnvim/lua/config/options.lua
+++ smallnvim/lua/config/options.lua
@@ -26,7 +26,7 @@ vim.o.relativenumber = true
 vim.o.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-vim.o.showmode = true
+vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
