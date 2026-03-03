f9be8538b2bce089150663d51a188b9da607398d
erikg <erik.grootoonk@gmail.com>
2026-03-03 22:02:59 +0100

    fzf-lua added

--- barenvim/nvim/lua/config/keymaps.lua
+++ barenvim/nvim/lua/config/keymaps.lua
@@ -15,3 +15,5 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- block select in WSL
+vim.keymap.set ('n', '<leader>v', '<c-v>')
