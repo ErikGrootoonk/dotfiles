7bf9e2c15162813ba4fbed9d17bf3c096dc7f005
Erik Grootoonk <erik.grootoonk@pinkroccade.nl>
2026-03-04 17:29:47 +0100

    install gruvbox in astronvim

--- astronvim/lua/plugins/colorscheme.lua
+++ astronvim/lua/plugins/colorscheme.lua
@@ -5,9 +5,14 @@ return {
     priority = 1000,
   },
   {
+     "ellisonleao/gruvbox.nvim", 
+     priority = 1000 , 
+     config = true,
+  },
+  {
     "AstroNvim/astroui",
     opts = {
-      colorscheme = "astrodark",
+      colorscheme = "gruvbox",
     },
   },
 }
