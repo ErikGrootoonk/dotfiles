087ba43718d3021c751faa3ca52671b91b0f39fc
Erik Grootoonk <erik.grootoonk@pinkroccade.nl>
2026-03-26 07:29:51 +0100

    updated keymaps

--- barenvim/nvim/lua/config/keymaps.lua
+++ barenvim/nvim/lua/config/keymaps.lua
@@ -10,13 +10,16 @@
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<cr>", { desc = "toggle Neotree", silent = true, noremap = true })
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
 
 -- block select in WSL
-vim.keymap.set ('n', '<leader>v', '<c-v>')
+vim.keymap.set ('n', '<leader>v', '<C-v>')
 
 -- jj to esc in insert mode
-vim.keymap.set('i', 'jj', '<Esc>', { noremap = true })
+vim.keymap.set({'i', 'v'}, 'jj', '<Esc>', { noremap = true })
+
+-- save file
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<Cmd>w<CR>', {desc = 'write file with ctrl-s', noremap = true, silent = true })
