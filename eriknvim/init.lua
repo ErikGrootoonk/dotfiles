6e6a9badd43193564765b9f6ac97fd8ed27389a2
ErikG <erik.grootoonk@gmail.com>
2026-03-28 06:24:51 +0100

    update

--- eriknvim/init.lua
+++ eriknvim/init.lua
@@ -59,6 +59,9 @@ lspconfig.lua_ls.setup({
   settings = {
     Lua = {
       runtime = { version = "LuaJIT" },
+      diagnostics = {
+        globals = { "vim" },  -- <-- add this
+      },
       workspace = {
         library = vim.api.nvim_get_runtime_file("", true),
         checkThirdParty = false,
