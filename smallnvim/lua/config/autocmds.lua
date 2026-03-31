b2d34741dc30f1ea9f7ef166494560a565432823
ErikG <erik.grootoonk@gmail.com>
2026-03-31 19:38:05 +0200

    smallnvim updated

---
+++ smallnvim/lua/config/autocmds.lua
@@ -0,0 +1,77 @@
+local api = vim.api
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("neoide_" .. name, { clear = true })
+end
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function() vim.highlight.on_yank() end,
+})
+
+-- Restore cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("restore_cursor"),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= line_count then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Close certain filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = { "help", "lspinfo", "man", "notify", "qf", "checkhealth" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- Auto-resize splits on terminal resize
+vim.api.nvim_create_autocmd("VimResized", {
+  group = augroup("resize_splits"),
+  callback = function() vim.cmd("tabdo wincmd =") end,
+})
+
+
+-- no auto continue comments on new line
+api.nvim_create_autocmd("FileType", {
+	group = api.nvim_create_augroup("no_autocomment", {}),
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
+
+
+-- restore cursor to file position in previous editing session
+api.nvim_create_autocmd("BufReadPost", {
+	callback = function(args)
+		local mark = api.nvim_buf_get_mark(args.buf, '"')
+		local line_count = api.nvim_buf_line_count(args.buf)
+		if mark[1] > 0 and mark[1] <= line_count then
+			api.nvim_win_set_cursor(0, mark)
+			-- defer centering slightly so it's applied after render
+			vim.schedule(function()
+				vim.cmd("normal! zz")
+			end)
+		end
+	end,
+})
+
+-- open help in vertical split
+api.nvim_create_autocmd("FileType", {
+	pattern = "help",
+	command = "wincmd L",
+})
+
+-- auto resize splits when the terminal's window is resized
+api.nvim_create_autocmd("VimResized", {
+	command = "wincmd =",
+})
+
+
